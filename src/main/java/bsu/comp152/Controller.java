cff1e836a05c225ccd8d224fcc2d501bf4010fa3
amanmarfatia <amarfatia@student.bridgew.edu>
2019-11-25 14:12:48 -0500

    Initial commit

---
+++ src/main/java/bsu/comp152/Controller.java
@@ -0,0 +1,30 @@
+package bsu.comp152;
+
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import javax.swing.text.html.ListView;
+
+public class Controller implements Initializable {
+    @FXML
+    private ListView<DataHandler.JokeDataType>ListControl;
+    private DataHandler Model;
+
+    public void loadData(){
+        var site = "https://sv443.net/jokeapi";
+        var params = getQueryParameters();
+        var query = site+params;
+
+        Model = new DataHandler(query);
+        var jokeList = Model.getData();
+        ObservableList<DataHandler.JokeDataType>dataToShow = FXCollections.observableArrayList(jokeList);
+        ListControl.setItems(dataToShow);
+    }
+    public String getQueryParameters(){
+        var joke = getJokesType();
+    }
+
+}
